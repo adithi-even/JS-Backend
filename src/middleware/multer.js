c6e9a9d324dcec496d43694bf2ff841f53414686
adithi-even <adithigopal1409@gmail.com>
2025-03-25 18:03:35 +0530

    addedd multer and continue on 7:40:40

---
+++ src/middleware/multer.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+
+const storage = multer.diskStojjrage({
+    destination: function (req, file, cb) {  // file(parameter) is already present in the multer so that we can use it to store the file
+      cb(null, "./public/temp")  //cb = call back
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)  //minor functionalities make it a good practice and ask gpt
+    }
+  })
+  
+  export const upload = multer({ 
+    storage,
+ })
