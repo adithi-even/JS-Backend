68297636b1494d5c9fea02c51063fc728f153317
adithi-even <adithigopal1409@gmail.com>
2025-04-19 19:07:56 +0530

    revising

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-
-
 const app = express()
 
-app.use(cors({  //TODO:WHAT IS CORS
+app.use(cors({  //TODO:WHAT IS CORS ? 
+  //Calling use(cors()) will enable the express server to respond to preflight requests. A preflight request is basically an OPTION request sent to the server before the actual request is sent, in order to ask which origin and which request options the server accepts.
     origin : process.env.CORS_ORIGIN,
     credentials : true
 }))
 
+//these all the below are different types methods in which we are getting the data into the backend
 app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended : true , limit : "16kb"})) //urlencoded is used to parse the incoming request with urlencoded payload
-app.use(express.static("public"))  //public is the folder name where the images are stored
+app.use(express.urlencoded({extended : true , limit : "16kb"})) //urlencoded is used to parse the incoming request with urlencoded payload i.e., when the data is sent through the url to the backend we have to pre-inform to the express that there will be data which ia also coming from the url so to handle that we use this urlencoded
+app.use(express.static("public"))  //public is the folder name where the images are stored in the server
 app.use(express.cookieParser())  //cookie parser is used to parse the incoming request with cookies
 
 
--- src/db/index.js
+++ src/db/index.js
@@ -11,7 +11,7 @@ const connectDB = async () =>{
                 
     } catch (error) {
         console.log("MONGODB connection Failed ",error);
-        process.exit(1)  //TODO : what is this 
+        process.exit(1)  //TODO : what is this ? In Node.js, process.exit(1) is used to terminate the process synchronously with an exit status of 1. This method instructs Node.js to exit immediately, even if there are still asynchronous operations pending, including I/O operations to process.stdout and process.stderr.
     }
 }
 
