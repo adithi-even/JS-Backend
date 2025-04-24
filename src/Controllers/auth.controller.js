1e75a98fe74d238875263ec0bec1a67b0f16340b
adithi-even <adithigopal1409@gmail.com>
2025-04-24 08:20:40 +0530

    controller

--- src/Controllers/auth.controller.js
+++ src/Controllers/auth.controller.js
@@ -1,5 +1,16 @@
 import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError";
 
 export const verifyJWT = asyncHandler(async(req, res, next)=>{
-    req.cooki
+  const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
+
+  if(!token) {
+    throw new ApiError(401, "Unauthorized request")
+  }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) //if the  token is present then we are going to check if the token is correct or not 
+
+  await User.find
+
 })
