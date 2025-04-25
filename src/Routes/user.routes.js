5a22acbc83343f87fa546d58cb784b33f0882332
adithi-even <adithigopal1409@gmail.com>
2025-04-25 08:24:12 +0530

    try to finish 1st full part of the video

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,6 +1,7 @@
-import {registerUser} from '../controllers/user.controller.js';
+import {loginUser, logoutUser, registerUser} from '../controllers/user.controller.js';
 import {Router} from 'express';
 import { upload } from '../middleware/multer.js';
+import { verifyJWT } from '../middleware/auth.middleware.js';
 
 const router = Router();    
 
@@ -22,5 +23,10 @@ router.route("/register").post(
 //since the code in the line 7th is the normal route code ,
 // but in this scenario we have a file uplaod feature so we are using the middleware (multer)- as the whole multer code is from the docs and is assigned to the "upload variable" so we are using the functionalities of the multer through the "upload variable"  , since we are putting the multer in between the router and post so that whatever the data is posted into the backend it is gonna meet with the middleware (multer i.e., code from 9-17) and also teh upload method do have various methods inside of it and we are again using all those as required.
 // and also if we want to add the multiple files into the db then we have to store multiplly , as we cannot use the array methos for upload.array because it takes and stores all the multiple data into the same field, so we are using upload.fields because it takes the multiple files associated with the given form fields
+
+router.route("/login").post(loginUser)
+
+//secured Routes
+router.route("/logout").post(verifyJWT, logoutUser) //if you want to understand this why we are suing the verifyJWT as the first parameter then watch JS Backend 55:00 and alos this line is linked to the "next()" in auth.middleware.js //check the video 
 
 export default router
---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,34 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError.js";
+import User from '../models/user.model.js'
+
+export const verifyJWT = asyncHandler(async(req, res, next)=>{
+  try {
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","") //TODO: ASK GPT
+  
+    if(!token) {
+      throw new ApiError(401, "Unauthorized request")
+    }
+  
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET) //if the  token is present then we are going to check if the token is correct or not //here 1st parameter is the token in the 6th line and the next parameter is the secret which is gonna verify that is this token belongs to us ? if the verification is true then we can store it in decodedToken 
+  
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+  
+    if(!user){
+      //TODO discuss about frontEnd
+      throw new ApiError(401, "Invalid Access Token")
+    }
+  
+    req.user = user;
+    next()
+    
+  } catch (error) {
+    throw new ApiError(401, error?.messgae || "Invaid Access TOken")
+  }
+
+
+
+
+
+})
