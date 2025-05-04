522ef6efe3041d62c7d196287b7a4d508cba0836
adithi-even <adithigopal1409@gmail.com>
2025-05-04 18:29:11 +0530

    1st part of aggretion pipeline is done next is the complex of aggretion pipeline

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -360,3 +360,68 @@ export const updateUserCoverImage = asyncHandler(async(req, res) => {
     
     return res.status(200).json(new ApiResponse(200, user, "Cover Image updated successfully "))
 })
+
+export const getUserChannelProfile = asyncHandler(async(req, res) => {
+    const {username} = req.params
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },{
+            $lookup:{
+                from: "sucscriptions" , //all the models will get lowercase and to plural , this Subscribtion  => subscriptions
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },{
+            $lookup:{
+                from: "sucscriptions" , //all the models will get lowercase and to plural , this Subscribtion  => subscriptions
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscriberdTo"
+            }
+        },{
+            $addFields:{
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed:{ //todo :learn
+                    $cond: {
+                        if:{$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+                
+            }
+        },{
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1 
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exist")
+    }
+
+    return res.status(200).json(new ApiResponse(200, channel[0], "user channel fetched successfully "))
+    
+})
